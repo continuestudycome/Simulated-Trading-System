901d625f73b8080fb238974b60630e37f05d572f
廖志强 <2448622080@qq.com>
2026-03-23 19:49:09 +0800

    初始化仓库

---
+++ Simulated-Trading-System/src/main/java/com/lzq/simulatedtradingsystem/SimulatedTradingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.lzq.simulatedtradingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulatedTradingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimulatedTradingSystemApplication.class, args);
+    }
+
+}
---
+++ Simulated-Trading-System/src/test/java/com/lzq/simulatedtradingsystem/SimulatedTradingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lzq.simulatedtradingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimulatedTradingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
